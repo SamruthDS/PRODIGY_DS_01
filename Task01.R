a36b1fb52390885aa380b6784cc0152d73807181
SamruthDS <samruthvadada@gmail.com>
2025-01-16 21:47:52 +0530

    Update Task01.R

--- Task01.R
+++ Task01.R
@@ -1,13 +1,8 @@
-# Set the working directory to the folder where your Titanic-Dataset.csv file is located
-setwd("C:/Users/ASUS/Downloads")  # Use forward slashes or double backslashes
-
-# Load necessary libraries
+setwd("C:/Users/ASUS/Downloads")  
 library(ggplot2)
-
-# Load the Titanic dataset
 titanic <- read.csv("Titanic-Dataset.csv")  # Ensure the file name matches exactly
 
-# Create a bar chart for the distribution of 'Sex'
+#Creating a bar chart for the distribution of 'Sex'
 ggplot(titanic, aes(x = Sex)) +
   geom_bar(fill = c("blue", "pink")) +
   labs(title = "Distribution of Sex in the Titanic Dataset",
